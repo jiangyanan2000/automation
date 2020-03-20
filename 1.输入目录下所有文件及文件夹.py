2ac606941feeee5a124372e142459b09153bfe0c
jiangyanan2000 <49583635@qq.com>
2020-03-21 00:38:42 +0800

    操作文件的函数

--- 1.输入目录下所有文件及文件夹.py
+++ 1.输入目录下所有文件及文件夹.py
@@ -1,1 +1,8 @@
-print("hello ")
+#获取当前Python程序的运行路径
+import os
+# print(os.getcwd())
+# print(os.path.join("Myprojects","AI"))
+# for i in os.listdir():
+#     print(i,os.path.isdir(i))
+for file in os.scandir():
+    print(file.name,file.is_dir())
